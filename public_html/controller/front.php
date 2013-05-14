98b7d78e84657861315c701d59e91ede8eb20372
mattimatti <mmonti@gmail.com>
2013-05-14 21:40:50 +0200

    Updated forms

--- public_html/controller/front.php
+++ public_html/controller/front.php
@@ -1,12 +1,21 @@
 <?php
 
 //HOME route show nothing
-$app->get('/', $noAuth(), function () use ($app) {
+$app->get('/manage', $noAuth(), function () use ($app) {
 
 			$data = array();
 			$app->render('index.html', $data);
 
 		});
+
+	$app->get('/', $noAuth(), function () use ($app) {
+
+		die(";)");
+
+	});
+
+
+
 
 /////////////////////// RESET //////////////////////////////////////////////
 
@@ -190,8 +199,8 @@ $app
 						R::store($answer);
 
 					}else{
-
-						throw new Exception("Risposta Non generata");
+						//throw new Exception("Risposta Non generata");
+						die("ko");
 					}
 
 					// If ajax request
@@ -240,10 +249,10 @@ $app
 					} else {
 
 						// Se stiamo simlando i forms
-						$simulate = $app->request()->post("simulate");
-						if ($simulate) {
-							$app->redirect("/app2/registeranswer");
-						}
+						//$simulate = $app->request()->post("simulate");
+						//if ($simulate) {
+						//	$app->redirect("/app2/registeranswer");
+						//}
 
 					}
 
@@ -259,91 +268,12 @@ $app
 
 				});
 
-//show a form to add metadata of the interviewed
+// Registra le risposte
 $app
-		->get('/start/:category_id/:step', $authApplication(),
-				function ($category_id, $step) use ($app) {
-
-					$interview = R::dispense("interview");
-
-					$interview->area = $_SESSION["area"];
-					$interview->region = $_SESSION["region"];
-					$interview->user_id = $_SESSION["user_id"];
-					$interview->last_access = R::isoDateTime();
-					$interview->questioncategory_id = $category_id;
-
-					R::store($interview);
-
-					$_SESSION["interview_id"] = $interview->id;
-
-					$app->redirect("/question/" . $category_id . "/" . $step);
-
-				});
-
-//show a form to add metadata of the interviewed
-$app->get('/end', $authApplication(), function () use ($app) {
+->get('/lastresponse', $noAuth(),
+		function () use ($app) {
 
 			$data = array();
-
-			$app->render('end.html', $data);
+			$app->render('lastresponse.html', $data);
 
 		});
-
-//show a form to add metadata of the interviewed
-$app
-		->get('/references', $authApplication(),
-				function () use ($app) {
-
-					$data = array();
-
-					$app->render('references.html', $data);
-
-				});
-
-//HOME route
-$app
-		->get('/question/:category_id/:step', $authApplication(),
-				function ($category_id, $step) use ($app) {
-
-					$category = R::findOne('questioncategory', ' id = :quid ', array(
-							':quid' => $category_id));
-
-					// fetch all the questions in this category
-					$questions = R::find('question', ' questioncategory_id = :questioncategory_id',
-							array(
-									':questioncategory_id' => $category_id));
-
-					// get the count
-					$questions_count = count($questions);
-
-					// find the next step
-					$nextstep = $step + 1;
-
-					//echo($nextstep);
-					// die();
-					$last = false;
-					// if not exceedes bounds walk to next ow go to root
-					if ($nextstep <= $questions_count) {
-						$nextPageName = '/question/' . $category_id . '/' . $nextstep . '';
-					} else {
-						$nextPageName = '/end';
-						$last = true;
-					}
-
-					// the question
-					$question = R::findOne('question', ' questioncategory_id = :questioncategory_id AND step = :step ',
-							array(
-									':questioncategory_id' => $category_id, ':step' => $step));
-
-					$data = array();
-					$data["nextpage"] = $nextPageName;
-					$data["question"] = $question;
-					//$data["answers"] = $answers;
-					$data["category"] = $category;
-					$data["last"] = $last;
-					$data["cases"] = R::find('questioncategory', ' id != :quid ', array(
-							':quid' => $category_id));
-
-					$app->render('question/question.html', $data);
-
-				});
