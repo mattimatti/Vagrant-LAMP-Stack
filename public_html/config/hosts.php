e6efd48d3668725897d3aab9b887ad51c4546658
mattimatti <mmonti@gmail.com>
2014-05-28 10:28:08 +0200

    aggiunto host

--- public_html/config/hosts.php
+++ public_html/config/hosts.php
@@ -8,7 +8,7 @@ $devlist = array('server.dev', '127.0.0.1');
 
 // for production environment
 
-$prodlist = array('waau.local', '33.33.33.90', '10.0.1.100');
+$prodlist = array('waau.local', '33.33.33.90', '10.0.1.100', '10.0.1.5');
 
 
 // for staging environments
